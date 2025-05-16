20c96502e62deea9f608ac1c67735a91d7e5f1a7
Sumit Kumar Singh <115483120+RAJPUTRoCkStAr@users.noreply.github.com>
2025-05-17 03:03:37 +0530

    Update HeroSection.tsx

--- src/components/Home/HeroSection.tsx
+++ src/components/Home/HeroSection.tsx
@@ -123,7 +123,7 @@ const HeroSection = () => {
                 initial={{ filter: 'blur(10px)' }}
                 animate={{ filter: 'blur(0px)' }}
                 transition={{ duration: 1, delay: 0.5 }}
-                src="https://images.pexels.com/photos/2379005/pexels-photo-2379005.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                src="https://github.com/RAJPUTRoCkStAr/portfolio-re/blob/main/home.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
                 alt="Sumit Singh"
                 className="w-full h-full object-cover"
               />
